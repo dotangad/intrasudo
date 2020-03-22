af6f6de2b634ac4f56afa5bf235efbc2a3f15c6c
Angad Singh <mail@angad.dev>
2020-03-22 21:07:12 +0530

    Add `exunite` and `admin` fields to User model.

--- migrations/20200322150556-user-isadmin-isexun.js
+++ migrations/20200322150556-user-isadmin-isexun.js
@@ -2,14 +2,16 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    queryInterface.addColumn("Users", "admin", {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false
-    });
-    queryInterface.addColumn("Users", "exunite", {
-      type: Sequelize.BOOLEAN,
-      defaultValue: false
-    });
+    return Promise.all([
+      queryInterface.addColumn("Users", "admin", {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      }),
+      queryInterface.addColumn("Users", "exunite", {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      })
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
@@ -20,7 +22,9 @@ module.exports = {
       Example:
       return queryInterface.dropTable('users');
     */
-    queryInterface.removeColumn("Users", "admin");
-    queryInterface.removeColumn("Users", "exunite");
+    return Promise.all([
+      queryInterface.removeColumn("Users", "admin"),
+      queryInterface.removeColumn("Users", "exunite")
+    ]);
   }
 };
